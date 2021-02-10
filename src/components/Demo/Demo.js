4413ee9f46a884c12a90668862752f865f4dbcd6
JosephAMParker <Joeyparker47@gmail.com>
2021-02-10 13:22:17 -0800

    demo subtext

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -1,9 +1,13 @@
-import React from 'react';  
+import React, {Fragment} from 'react';  
+import './Demo.scss'
 
 function Header(props) {
 
 	return ( 
-		<h1 class="text-center"> <a href="https://josephamparker.github.io/neowise-game/">PLAY NOW</a> </h1> 
+		<Fragment>
+			<h1 class="text-center"> <a href="https://josephamparker.github.io/neowise-game/">PLAY DEMO*</a> </h1> 
+			<p class="subtext"> * Work In Progress- Does not represent the final look of the game. The finished product will be released for Android and iOS NOT in browser. You may experience performance issues playing in browser. </p>
+		</Fragment>
 	)
 
 }
